12b1a72c17b9537587a40cb8f3efc8cd977b3723
anhtuansggd <122171727+anhtuansggd@users.noreply.github.com>
2024-03-28 23:51:17 +0700

    Backend Setup (#1)
    * build: setup backend folder
    
    * feat: implement authentication
    
    - Configure authentication manager
    
    - Implement GrantAuthority and UserDetailsService
    
    * feat: add JwtFilter and AuthEndpoint
    
    * feat: add validate endpoint for LoginController
    
    * test: add tests for controllers and service
    
    - Tests for: LoginController, UserDetailsServiceImpl
    - Add testcontainer with postgresql image

---
+++ backend/src/main/java/com/petpal/backend/domain/User.java
@@ -0,0 +1,52 @@
+package com.petpal.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new Authority("ROLE_PETOWNER"));
+        return roles;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
