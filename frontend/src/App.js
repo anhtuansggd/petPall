7b96794fa281743a451f91f2c94874d3067510f2
anhtuansggd <anhtuan.sggd@gmail.com>
2024-04-05 10:18:22 +0700

    feat: create Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import {useLocalState} from "./util/useLocalStorage";
 import {useEffect} from "react";
-
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Homepage from "./Homepage";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
 function App() {
     const [jwt, setJwt] = useLocalState("","jwt");
 
@@ -28,9 +32,15 @@ function App() {
 
 
     return(
-      <div className="App">
-        <h1>Jwt values is {jwt}</h1>
-      </div>
+        <Routes>
+            <Route path="/dashboard" element={
+                <PrivateRoute>
+                    <Dashboard/>
+                </PrivateRoute>
+            }/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/" element={<Homepage/>}/>
+        </Routes>
   );
 }
 
